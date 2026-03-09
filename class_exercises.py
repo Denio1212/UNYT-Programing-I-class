b7be80b68be88b9f910401f06c260812b85708de
Denio1212 <shehuden22@yahoo.com>
2026-03-09 18:40:14 +0100

    Finalised Class Exercises for the first week

--- class_exercises.py
+++ class_exercises.py
@@ -6,25 +6,29 @@ def decimal_value():
     numerator = int(input("Enter a numerator: "))
     denominator = int(input("Enter a denominator: "))
     dec_value = numerator / denominator
-    print(dec_value.__float__())
+    print(f"{dec_value:.2f}")
 
 
 def area_of_circle():
     """
     Task 7
+    Simple exercise just dealing with floats
     """
     pi = 3.14159
     radius = float(input("Enter a radius: "))
     area = pi * radius * radius
-    print(area)
+    print(f"The area of the circle is: {area:.2f}")
 
 
 def round_the_number():
     """
     Task 8
+    Using the format method, which is the nicest way for multi-variable printing
     """
-    number = float(input("Enter a number: "))
+    number = float(input("Enter a decimal number: "))
     rounded_number = round(number)
     print("{0}, rounded off to the nearest int is {1}".format(number, rounded_number))
 
+decimal_value()
+area_of_circle()
 round_the_number()
